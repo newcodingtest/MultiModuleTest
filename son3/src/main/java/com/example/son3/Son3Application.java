aadcf634259d5d16e22d764b848ba5aec9a0d484
yjy <yjy>
2022-09-30 15:51:06 +0900

    test3

--- son3/src/main/java/com/example/son3/Son3Application.java
+++ son3/src/main/java/com/example/son3/Son3Application.java
@@ -3,6 +3,7 @@ package com.example.son3;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//test3
 @SpringBootApplication
 public class Son3Application {
 
