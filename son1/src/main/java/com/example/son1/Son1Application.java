b1d926e5c68b686e83e9aa059ac233c4b4722bfd
yjy <yjy>
2022-09-30 15:53:15 +0900

    test1

--- son1/src/main/java/com/example/son1/Son1Application.java
+++ son1/src/main/java/com/example/son1/Son1Application.java
@@ -3,6 +3,7 @@ package com.example.son1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//test1
 @SpringBootApplication
 public class Son1Application {
 
