fa9f3163da608ecbbe9bc0ad45b181f14526d824
newcodingtest <pulpul8282@naver.com>
2022-10-12 13:23:11 +0900

    test12

--- son2/src/main/java/com/example/son2/Son2Application.java
+++ son2/src/main/java/com/example/son2/Son2Application.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 //test2
-//젠킨스야 잡아라     
+//젠킨스야 잡아라1
+//젠킨스야 잡아라2
 @SpringBootApplication
 public class Son2Application {
 
---
+++ son2/src/main/java/com/example/son2/TEST2.java
@@ -0,0 +1,4 @@
+package com.example.son2;
+
+public class TEST2 {
+}
